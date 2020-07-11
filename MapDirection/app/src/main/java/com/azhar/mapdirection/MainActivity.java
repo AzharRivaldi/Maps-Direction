008ce5c8148d1a1ff07dbb4ef444e7645eb3204e
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2020-07-11 12:53:44 +0700

    Update MainActivity.java

--- MapDirection/app/src/main/java/com/azhar/mapdirection/MainActivity.java
+++ MapDirection/app/src/main/java/com/azhar/mapdirection/MainActivity.java
@@ -40,80 +40,54 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
     GoogleMap mGoogleMap;
-    ProgressBar pBar;
-    double mLatitude = 0;
-    double mLongitude = 0;
+    ProgressBar pbMap;
+    Spinner spWisata;
+    String [] sWisata = {"mosque", "restaurant", "atm", "bank", "school"
+            , "hospital", "laundry", "university", "post_office", "police"};
+    String xWisata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setID();
 
-        Spinner spinnerCari = findViewById(R.id.spnCari);
-        pBar = findViewById(R.id.pBar);
-
-        SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        fragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                mGoogleMap = googleMap;
-                initMap();
-            }
-        });
-
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(MainActivity.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.cari_tempat));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerCari.setAdapter(myAdapter);
-        spinnerCari.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-
-                //daftar pilihan spinner
-                String xType = "";
-                if (position == 1)
-                    xType = "mosque";
-                else if (position == 2)
-                    xType = "restaurant";
-                else if (position == 3)
-                    xType = "atm";
-                else if (position == 4)
-                    xType = "bank";
-                else if (position == 5)
-                    xType = "school";
-                else if (position == 6)
-                    xType = "hospital";
-                else if (position == 7)
-                    xType = "laundry";
-                else if (position == 8)
-                    xType = "university";
-                else if (position == 9)
-                    xType = "post_office";
-                else if (position == 10)
-                    xType = "police";
-                if (position != 0) {
-
-                    //place API
-                    String sb = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
-                            "location=" + mLatitude + "," + mLongitude +
-                            "&radius=5000" +
-                            "&types=" + xType +
-                            "&sensor=true" +
-                            "&key=" + getResources().getString(R.string.google_maps_key);
-                    startProg();
-                    new PlacesTask().execute(sb);
-                }
+        spWisata.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                Object itemDB = parent.getItemAtPosition(pos);
+                xWisata = itemDB.toString();
+                SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fMap);
+                fragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        mGoogleMap = googleMap;
+                        initMap();
+                    }
+                });
+                startProg();
             }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
+            public void onNothingSelected(AdapterView<?> parent) { }
         });
     }
 
-    private void initMap() {
+    private void setID(){
+        pbMap = findViewById(R.id.pbMap);
+        spWisata = findViewById(R.id.spWisata);
+        ArrayAdapter<String> adpWisata = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sWisata);
+        adpWisata.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spWisata.setAdapter(adpWisata);
+    }
 
-        //cek permission
+    private void stopProg() {
+        pbMap.setVisibility(View.GONE);
+    }
+
+    private void startProg() {
+        pbMap.setVisibility(View.VISIBLE);
+    }
+
+    private void initMap() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 115);
@@ -139,26 +113,19 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
-        mLatitude = location.getLatitude();
-        mLongitude = location.getLongitude();
+        double mLatitude = location.getLatitude();
+        double mLongitude = location.getLongitude();
         LatLng latLng = new LatLng(mLatitude, mLongitude);
-        /*Circle circle = mGoogleMap.addCircle(new CircleOptions()
-                .center(new LatLng(mLatitude, mLongitude))
-                .radius(500)
-                .strokeWidth(6)
-                .strokeColor(0xffff0000)
-                .fillColor(0x55ff0000));*/
         mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+
+        String sb = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
+                "location=" + mLatitude + "," + mLongitude +
+                "&radius=20000" +
+                "&types=" + xWisata +
+                "&key=" + getResources().getString(R.string.google_maps_key);
+        new MainActivity.PlacesTask().execute(sb);
         stopProg();
-    }
-
-    private void stopProg() {
-        pBar.setVisibility(View.GONE);
-    }
-
-    private void startProg() {
-        pBar.setVisibility(View.VISIBLE);
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -177,7 +144,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         @Override
         protected void onPostExecute(String result) {
-            new ParserTask().execute(result);
+            new MainActivity.ParserTask().execute(result);
         }
     }
 
@@ -213,7 +180,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @SuppressLint("StaticFieldLeak")
     private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String, String>>> {
         JSONObject jObject;
-
         @Override
         protected List<HashMap<String, String>> doInBackground(String... jsonData) {
             List<HashMap<String, String>> places = null;
@@ -228,7 +194,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             return places;
         }
 
-        //untuk menampilkan jumlah lokasi terdekat
         @Override
         protected void onPostExecute(List<HashMap<String, String>> list) {
             mGoogleMap.clear();
@@ -253,15 +218,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-    }
-
+    public void onStatusChanged(String s, int i, Bundle bundle) { }
     @Override
-    public void onProviderEnabled(String s) {
-    }
-
+    public void onProviderEnabled(String s) { }
     @Override
-    public void onProviderDisabled(String s) {
-    }
+    public void onProviderDisabled(String s) { }
 
 }
